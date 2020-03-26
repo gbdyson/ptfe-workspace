4ef431185d21a44f6e57cb538e923f5e0d08d448
gbdyson <36828380+gbdyson@users.noreply.github.com>
2020-03-26 10:44:48 -0400

    Update main.tf

--- app-vm-dev/main.tf
+++ app-vm-dev/main.tf
@@ -3,6 +3,9 @@
 # export VSPHERE_USER=""
 # export VSPHERE_PASSWORD=""
 provider "vsphere" {
+    user               = var.VSPHERE_USER
+  password             = var.VSPHERE_PASSWORD
+  vsphere_server       = var.VSPHERE_SERVER
   allow_unverified_ssl = true
 }
 
